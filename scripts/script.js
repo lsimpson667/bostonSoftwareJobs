3e3055429d3b1303eb5048ea66c7dae73c10420b
Lois Simpson <lsimpson667@gmail.com>
2020-04-05 22:15:25 -0400

    Part2

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,28 @@
-let txt2 = "";
+let companyList = "";
 
 function showPromptForLogoUrl() {
+    let addCompanyName = prompt("Enter the name of the company.");
+    let companyNameArr = [];
+    companyNameArr.push(addCompanyName);
 
-    let addACompany = prompt("Enter the URL to a corporate logo.");
 
-    if (addACompany === null || addACompany === undefined || addACompany === "" || !addACompany.includes("http")) {
-        document.getElementById("showCorporateLogo").innerHTML = txt2;
+    let addCompanyLogo = prompt("Enter the URL to a corporate logo.");
+    let companyLogoArr = [];
+    companyLogoArr.push(addCompanyLogo);
+
+    let addCompanyDescription = prompt("Write a breif description of the company.")
+    let companyDescriptionArr = [];
+    companyDescriptionArr.push(addCompanyDescription);
+
+
+    if (addCompanyLogo === null || addCompanyLogo === undefined || addCompanyLogo === "" || !addCompanyLogo.includes("http")) {
+        document.getElementById("showCorporateLogo").innerHTML = companyList;
     } else {
-    txt2 += `<div><img src='${addACompany}' width='250px'></div>`;
-    document.getElementById("showCorporateLogo").innerHTML = txt2;
+        for (let i = 0; i < companyNameArr.length; i ++) {
+        companyList += `<div class='individualLogoContainers img-thumbnail'><div class=''><img src='${companyLogoArr[i]}' width='250px' class='image'></div><div class=''><p>${companyNameArr[i]}</p></div><div class=''><p>${companyDescriptionArr[i]}</p></div></div>`;
+        }
+    document.getElementById("showCorporateLogo").innerHTML = companyList;
     }
+
     
 }
